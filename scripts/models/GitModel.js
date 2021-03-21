31b1cb3f8954ecd67fd66f604df19312b50e901f
anvitrola <vitoriavianathiss@outlook.com>
2021-03-20 23:15:00 -0300

    Fechando a requisição na API e colocando toda a informação requerida dentro de um array. Ã requisição está sendo feita de forma síncrona, mas falta criar uma callback para que o processo seja feito de uma maneira melhor para garantir a usabilidade do usuário

--- scripts/models/GitModel.js
+++ scripts/models/GitModel.js
@@ -4,13 +4,13 @@ class GhModel {
     }
     openRequest (){
         let request = new XMLHttpRequest;
-        request.open('GET', 'https://api.github.com/orgs/grupotesseract/public_members');
+        request.open('GET', 'https://api.github.com/orgs/grupotesseract/public_members', false);
         request.addEventListener('load', () => {
             let result = JSON.parse(request.responseText);
             if (request.status == 200){
                result.forEach((object) => {
                     let memberRequest = new XMLHttpRequest;
-                    memberRequest.open('GET', `https://api.github.com/users/${object.login}`);
+                    memberRequest.open('GET', `https://api.github.com/users/${object.login}`, false);
                     memberRequest.addEventListener('load', () => {
                         let memberResult = JSON.parse(memberRequest.responseText);
                         if(memberRequest.status == 200){
@@ -23,7 +23,6 @@ class GhModel {
                                 date: new Date(memberResult.created_at)
                             }
                             this.team.push(member);
-                            console.log(this.team);
                         } else{
                             throw new Error(`Request error ${memberRequest.status}: ${memberRequest.statusText}.`);
                         }
@@ -36,4 +35,5 @@ class GhModel {
         })
         request.send();
     }
+
 }
